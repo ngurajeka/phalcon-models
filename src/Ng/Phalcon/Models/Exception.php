963c89eebc5914ba771be43a8080ae0b128009e3
Ady Rahmat MA <ganteng@ngurajeka.com>
2016-07-07 22:29:30 +0700

    separate models from crud

---
+++ src/Ng/Phalcon/Models/Exception.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Model Exception
+ *
+ * PHP Version 5.4.x
+ *
+ * @category Library
+ * @package  Library
+ * @author   Ady Rahmat MA <adyrahmatma@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/ngurajeka/phalcon-crud
+ */
+namespace Ng\Phalcon\Models;
+
+
+/**
+ * Model Exception
+ *
+ * @category Library
+ * @package  Library
+ * @author   Ady Rahmat MA <adyrahmatma@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/ngurajeka/phalcon-crud
+ */
+class Exception extends \Exception
+{
+}
